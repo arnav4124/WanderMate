a78d0230b4cf7ef28a78b2a02626878c08302a90
aroy <royayush2005@gmail.com>
2026-04-23 03:35:41 +0530

    Removed garbage files

--- test_ors10.js
+++
@@ -1,9 +0,0 @@
-const { default: axios } = require('axios');
-(async () => {
-    try {
-        const response = await axios.post('http://localhost:5001/api/routes/optimize', { coordinates: [{ lat: 48.8566, lng: 2.3522 }, { lat: 48.8584, lng: 2.2945 }, { lat: 48.8606, lng: 2.3376 }] }, { headers: { 'Content-Type': 'application/json' } });
-        console.log(response.data);
-    } catch (e) {
-        console.log(e.response ? e.response.data : e.message);
-    }
-})();
